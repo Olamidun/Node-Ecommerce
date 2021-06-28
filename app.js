56cb27215683ff60d15f8e6553446727b67816cd
Olamidun <kolapoolamidun@gmail.com>
2021-06-28 16:43:46 +0100

    logging out port to debug

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 const port = process.env.PORT || 5000
+console.log(port)
 try{
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
     // mongodb+srv://8SgLP8Dhd1PJ3F21@@nodeecommerce.qmtrr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
