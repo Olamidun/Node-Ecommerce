2f238d4516706baf1cc86ad17eef396309569b8d
Olamidun <kolapoolamidun@gmail.com>
2021-06-28 16:04:26 +0100

    installed cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const userRouter = require('./api/routes/userRouter')
 const productRouter = require('./api/routes/productRouter')
@@ -7,6 +8,7 @@ const orderRouter = require('./api/routes/orderRouter')
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 const port = process.env.PORT || 5000
